e1573917e67e141079ca11426d7785c4b00488bb
jacksmithinsulander <666.jack.smith@protonmail.com>
2026-01-09 17:13:41 +0100

    second part forth day

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,6 +1,13 @@
 use std::fs;
 
 fn main() {
+    println!("Problem one:");
+    prob_one();
+    println!("Problem two:");
+    prob_two();
+}
+
+fn prob_one() {
     let input = fs::read_to_string("src/input.txt").expect("Failed to read input");
 
     let mut result = 0;
@@ -13,7 +20,6 @@ fn main() {
             if char != &'@' {
                 continue;
             }
-
             if char_index == 0 {
                 if chars[char_index + 1] == '@' {
                     adjecent = adjecent + 1
@@ -139,6 +145,161 @@ fn main() {
             if adjecent < 4 {
                 result = result + 1
             }
+        }
+    }
+    println!("Its {}", result);
+}
+
+fn prob_two() {
+    let input = fs::read_to_string("src/input.txt").expect("Failed to read input");
+
+    let mut input_mut: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect();
+
+    let mut result = 0;
+
+    loop {
+        let mut change = false;
+        for (line_index, line) in input_mut.clone().iter().enumerate() {
+            for (char_index, char) in line.iter().enumerate() {
+                let mut adjecent = 0;
+                if char != &'@' {
+                    continue;
+                }
+
+                if char_index == 0 {
+                    if line[char_index + 1] == '@' {
+                        adjecent = adjecent + 1
+                    }
+                } else if char_index == line.len() - 1 {
+                    if line[char_index - 1] == '@' {
+                        adjecent = adjecent + 1
+                    }
+                } else {
+                    if line[char_index - 1] == '@' {
+                        adjecent = adjecent + 1
+                    }
+                    if line[char_index + 1] == '@' {
+                        adjecent = adjecent + 1
+                    }
+                }
+
+                if line_index == 0 {
+                    let next_chars: Vec<char> = input_mut[line_index + 1].clone();
+                    if char_index == 0 {
+                        if next_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    } else if char_index == line.len() - 1 {
+                        if next_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    } else {
+                        if next_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    }
+                } else if line_index == input_mut.len() - 1 {
+                    let prev_chars: Vec<char> = input_mut[line_index - 1].clone();
+                    if char_index == 0 {
+                        if prev_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    } else if char_index == line.len() - 1 {
+                        if prev_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    } else {
+                        if prev_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    }
+                } else {
+                    let prev_chars: Vec<char> = input_mut[line_index - 1].clone();
+                    let next_chars: Vec<char> = input_mut[line_index + 1].clone();
+                    if char_index == 0 {
+                        if prev_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    } else if char_index == line.len() - 1 {
+                        if prev_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    } else {
+                        if prev_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if prev_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index - 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                        if next_chars[char_index + 1] == '@' {
+                            adjecent = adjecent + 1
+                        }
+                    }
+                }
+
+                if adjecent < 4 {
+                    result = result + 1;
+                    input_mut[line_index][char_index] = '.';
+                    change = true;
+                }
+            }
+        }
+        if !change {
+            break;
         }
     }
     println!("Its {}", result);
